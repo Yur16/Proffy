971f63532c9a2e84549a1002737d1650dad871ce
Yuri <yurioliveira@aluno.fapce.edu.br>
2020-08-06 21:28:21 -0300

    feat: integrando aplicação web com a API

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -3,33 +3,52 @@ import React from 'react';
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css';
+import api from '../../services/api';
 
-const TeacherItem: React.FC = () => {
+export interface Teacher {
+  id: number;
+  avatar: string;
+  bio: string;
+  cost: number;
+  name: string;
+  subject: string;
+  whatsapp: string;
+}
+
+interface TeacherItemProps {
+  teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+  function createNewConnection() {
+    api.post('connections', {
+      user_id: teacher.id,
+    });
+  }
+
   return (
     <article className="teacher-item">
       <header>
-        <img src="https://avatars2.githubusercontent.com/u/45566439?s=460&u=3fd5a4801a016fbf8f33823d7b049926a98ddfb6&v=4" alt="Yuri Oliveira" />
+        <img src={teacher.avatar} alt={teacher.name} />
         <div>
-          <strong>Yuri OLivera</strong>
-          <span>Matemática</span>
+          <strong>{teacher.name}</strong>
+          <span>{teacher.subject}</span>
         </div>
       </header>
 
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nisi urna, viverra ut urna vel, pellentesque luctus elit.
-        <br /><br />
-        Vestibulum fermentum tristique cursus. Donec at dui tortor. Vestibulum sit amet accumsan sapien. In sit amet turpis elit. Donec volutpat, orci quis elementum iaculis, ante urna semper ante, pellentesque tempor ligula nisi a metus.
+        {teacher.bio}
       </p>
 
       <footer>
         <p>
           Preço/hora
-          <strong>R$ 80,00</strong>
+          <strong>R$ {teacher.cost}</strong>
         </p>
-        <button type="button">
+        <a target="_blank" onClick={createNewConnection} href={`https://wa.me/${teacher.whatsapp}`}>
           <img src={whatsappIcon} alt="whatsapp" />
             Entrar em contato
-        </button>
+        </a>
       </footer>
     </article>
   );
